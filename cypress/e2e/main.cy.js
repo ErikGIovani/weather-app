3485b64dbaf6b91153ad72ce41adc48d66be4935
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2023-02-27 13:13:08 -0600

    refactor: test 32e

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,27 +1,26 @@
 describe("See the weather of the Mexico City", () => {
   it("Should find the Mexico City text in the page and need to be all in english", () => {
-    cy.visit("http://localhost:3000");
-    cy.wait(5000);
+    cy.visit("/");
+    cy.contains(/Loading.../i);
     cy.contains("Mexico City, MX");
     cy.contains("No Favorite Places");
     cy.contains("Minimum Temperature");
     cy.contains("Humidity");
     cy.contains("Maximum Temperature");
-    cy.contains(/°C/i);
+    cy.findAllByText(/°C/i).should("have.length", 8);
     cy.findByRole("button", { name: "ES" }).click();
-    cy.wait(5000);
+    cy.contains(/Cargando.../i);
     cy.contains("No Hay Lugares Favoritos");
     cy.contains("Temperatura Mínima");
     cy.contains("Humedad");
     cy.contains("Temperatura Máxima");
     cy.findByRole("button", { name: "°F" }).click();
-    cy.wait(5000);
-    cy.contains(/°F/i);
+    cy.findAllByText(/°F/i).should("have.length", 8);
     cy.get(".Sidebar_favorite_icon__zvQi1").click();
-    cy.get(".Form_input__CkksV").type("mexico");
+    cy.findAllByText(/mexico city/i).should("have.length", 2);
+    cy.get(".Form_input__CkksV").type("mexico,");
     cy.wait(1000);
     cy.contains("Mexico, US").click();
-    cy.wait(5000);
     cy.contains("Mexico, US");
   });
 });
